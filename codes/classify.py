b13610b05e225827eefb41115aa330994165bf20
Bala93 <balamuralim.1993@gmail.com>
2018-07-06 01:36:46 +0530

    Added model files to check with other models

--- codes/classify.py
+++ codes/classify.py
@@ -230,7 +230,7 @@ if __name__ == "__main__":
     # Decay LR by a factor of 0.1 every 7 epochs
 
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
-    model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,save_path,num_epochs=1)
+    model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,save_path,num_epochs=30)
     logging.info("Ends here")
 
     # visualize_model(model_ft)

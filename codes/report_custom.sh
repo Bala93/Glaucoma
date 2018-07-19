9e81f50ba46a11f67a5cd91123fb617771f478df
viggi1000 <viggi1000@gmail.com>
2018-07-19 15:44:17 +0530

    in report_custom resolved errors

--- codes/report_custom.sh
+++ codes/report_custom.sh
@@ -1,17 +1,17 @@
-src_train_path='/media/htic/NewVolume1/murali/Glaucoma/PretrainDataSets/Combined_RimOne_Origa'
-src_val_path='/media/htic/NewVolume1/murali/Glaucoma/Refuge'
-src_save_path='/media/htic/Balamurali/Glaucoma_models'
+src_train_path='/media/hticdeep/drive3/Balamurali/Glaucoma/PretrainDataSets/Combined_RimOne_Origa'
+src_val_path='/media/hticdeep/drive3/Balamurali/Glaucoma/Refuge'
+src_save_path='/media/hticdeep/Balamurali/Glaucoma_models'
 cuda_no=0
 no_classes=2
 is_pretrained=1
 
-for model_name in 'resnet101' 'resnet152' 'densenet169' 'densenet201' 'vgg16_bn' 'vgg19_bn' 'inception'
+for model_name in 'resnet101' 'resnet152' 'densenet169' 'densenet201' #'vgg16_bn' 'vgg19_bn' 'inception'
  do
   for color in 'LAB' 'Normalized' 'PseudoDepth'
    do 
    train_path=${src_train_path}/${color}
    val_path=${src_val_path}/${color}
    save_path=${src_save_path}/${model_name}_${color}
-   echo python classify.py --train_path ${train_path} --val_path ${val_path} --save_path ${save_path} --cuda_no ${cuda_no} --model_name ${name} --no_classes ${no_classes} --is_pretrained ${is_pretrained}
+   python classify_custom.py --train_path ${train_path} --val_path ${val_path} --save_path ${save_path} --cuda_no ${cuda_no} --model_name ${model_name} --no_classes ${no_classes} --is_pretrained ${is_pretrained}
    done
  done
